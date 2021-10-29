087357d3b28a4dfa1d7a8ec884d4527b408b8f5a
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2021-10-29 18:10:27 -0500

    Insert new user in database

--- resources/signupprocess.php
+++ resources/signupprocess.php
@@ -1,3 +1,20 @@
 <?php
+include_once("functions.php");
 
-header("Location: ../index.php?type=success&message=Signed up successfully");
+$lastname = $_POST['lastname'];
+$firstname = $_POST['firstname'];
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+
+$conn = connectDB();
+
+$query = "insert into user(username, password, firstname, lastname) values('$username', '$password', '$firstname', '$lastname');";
+$result = $conn->query($query);
+
+disconnectDB($conn);
+
+if ($result) {
+	header("Location: ../index.php?type=success&message=Signed up successfully");
+} else {
+	header("Location: ../signup.php?type=error&message=Username already exists");
+}
