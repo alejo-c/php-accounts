69080f1eeffe2858b91f3c7a5f9a8cb608c0af1f
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2021-10-29 18:19:48 -0500

    Start session as logged user from database

--- resources/loginprocess.php
+++ resources/loginprocess.php
@@ -1,3 +1,29 @@
 <?php
+$result = session_start();
+require_once("functions.php");
 
-header("Location: ../index.php");
+$username = $_POST["username"];
+$password = md5($_POST["password"]);
+
+$conn = connectDB();
+
+$query = "select username, password, role, firstname, lastname from user where username='$username' and password='$password';";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+	$row = $result->fetch_assoc();
+
+	$_SESSION = array(
+		'id' => session_id(),
+		'username' => $username,
+		'role' => $row['role'],
+		'firstname' =>  $row['firstname'],
+		'lastname' => $row['lastname']
+	);
+
+	header("Location: ../index.php?type=success&message=Logged in successfully");
+} else {
+	header("Location: ../login.php?type=error&message=Wrong username or password");
+}
+
+disconnectDB($conn);
