f2f247ce66c20c2fa14cf6ce24080f25adf7c97e
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2021-10-31 19:22:06 -0500

    Remove invalid input characters at type
    Restrinct the valid characters in the input fields of forms.

--- components/navbar.php
+++ components/navbar.php
@@ -1,41 +1,33 @@
 <?php
-require_once("./resources/functions.php")
+require_once("./resources/mainfunctions.php");
 ?>
 
-<html>
+<link rel="stylesheet" href="css/navbar.css">
 
-<head>
-	<link rel="stylesheet" href="css/navbar.css">
-</head>
+<nav class="main-nav">
+	<ul class="nav-start">
+		<li <?php echo active_when("index"); ?>>
+			<a href="." class="nav-item">Home</a>
+		</li>
+	</ul>
+	<ul class="nav-end">
+		<li <?php echo active_when("login");
+			echo hidden_when("LOGGED"); ?>>
+			<a href="login.php" class="nav-item">Login</a>
+		</li>
 
-<body>
-	<nav class="main-nav">
-		<ul class="nav-start">
-			<li <?php echo active_when("index"); ?>>
-				<a href="." class="nav-item">Home</a>
-			</li>
-		</ul>
-		<ul class="nav-end">
-			<li <?php echo active_when("login");
-				echo hidden_when("LOGGED"); ?>>
-				<a href="login.php" class="nav-item">Login</a>
-			</li>
+		<li <?php echo active_when("signup");
+			echo hidden_when("LOGGED"); ?>>
+			<a href="signup.php" class="nav-item">Sign up</a>
+		</li>
 
-			<li <?php echo active_when("signup");
-				echo hidden_when("LOGGED"); ?>>
-				<a href="signup.php" class="nav-item">Sign up</a>
-			</li>
+		<li <?php echo active_when("profile");
+			echo hidden_when("UNLOGGED"); ?>>
+			<a href="profile.php" class="nav-item">Profile</a>
+		</li>
 
-			<li <?php echo active_when("profile");
-				echo hidden_when("UNLOGGED"); ?>>
-				<a href="profile.php" class="nav-item">Profile</a>
-			</li>
-
-			<li <?php echo hidden_when("UNLOGGED"); ?>>
-				<a href="./resources/logoutprocess.php" class="nav-item">log out</a>
-			</li>
-		</ul>
-	</nav>
-</body>
-
-</html>
+		<li <?php echo hidden_when("UNLOGGED"); ?>>
+			<a href="resources/db/logoutprocess.php" class="nav-item">log out</a>
+		</li>
+	</ul>
+</nav>
--- components/profileform.php
+++ components/profileform.php
@@ -3,48 +3,42 @@ $username = isset($_POST['new-username']) ? $_POST['new-username'] : $_SESSION['
 $firstname = isset($_POST['new-firstname']) ? $_POST['new-firstname'] : $_SESSION['firstname'];
 $lastname = isset($_POST['new-lastname']) ? $_POST['new-lastname'] : $_SESSION['lastname'];
 ?>
-<html>
+<link rel="stylesheet" href="css/form.css">
 
-<head>
-	<link rel="stylesheet" href="css/form.css">
-</head>
+<div class="form-container">
+	<header>
+		<h1 class="title">Update Profile</h1>
+		<h2 class="title"><?php echo "$username"; ?></h2>
+	</header>
+	<p>
+		Update your account information and introduce your password to confirm.
+	</p>
+	<form action="resources/db/updateprocess.php" method="post">
+		<div class="form-group">
+			<input type="text" name="new-username" minlength="5" maxlength="20" placeholder="Enter new Username" value="<?php echo $username; ?>" onkeyup="this.value=this.value.replace(/[^_a-zA-Z0-9]/g, '')">
+		</div>
 
-<body>
-	<div class="form-container">
-		<header>
-			<h1 class="title">Update Profile</h1>
-			<h2 class="title"><?php echo "$username - $firstname $lastname"; ?></h2>
-		</header>
-		<p>
-			Update your account information and introduce<br>your password to confirm.
-		</p>
-		<form action="./resources/updateprocess.php" method="post">
+		<div>
 			<div class="form-group">
-				<input type="text" name="new-username" minlength="5" maxlength="20" placeholder="Enter new Username" value="<?php echo $username; ?>">
-			</div>
-
-			<div class="form-group">
-				<input type="text" name="new-lastname" minlength="2" maxlength="20" placeholder="Enter new Lastname" value="<?php echo $lastname; ?>">
+				<input type="text" name="new-lastname" minlength="2" maxlength="20" placeholder="Enter new Lastname" value="<?php echo $lastname; ?>" onkeyup="this.value=this.value.replace(/[^ a-zA-Z0-9À-ÿ\u00f1\u00d1]/g, '')">
 			</div>
 
 			<div class="form-group">
-				<input type="text" name="new-firstname" minlength="2" maxlength="20" placeholder="Enter new Firstname" value="<?php echo $firstname; ?>">
+				<input type="text" name="new-firstname" minlength="2" maxlength="20" placeholder="Enter new Firstname" value="<?php echo $firstname; ?>" onkeyup="this.value=this.value.replace(/[^ a-zA-Z0-9À-ÿ\u00f1\u00d1]/g, '')">
 			</div>
+		</div>
 
-			<div class="form-group">
-				<input type="password" name="new-password" minlength="5" maxlength="20" placeholder="Enter new Password" autocomplete="new-password">
-			</div>
+		<div class="form-group">
+			<input type="password" name="new-password" minlength="5" maxlength="20" placeholder="Enter new Password" autocomplete="new-password">
+		</div>
 
-			<div class="form-group">
-				<input type="password" name="password" minlength="5" maxlength="20" placeholder="Enter current Password" required>
-			</div>
+		<div class="form-group">
+			<input type="password" name="password" minlength="5" maxlength="20" placeholder="Enter current Password" required>
+		</div>
 
-			<div class="btn-group">
-				<button type="submit" class="btn"><span>Update</span></button>
-				<button type="reset" class="btn"><span>Reset</span></button>
-			</div>
-		</form>
-	</div>
-</body>
-
-</html>
+		<div class="btn-group">
+			<button type="submit" class="btn"><span>Update</span></button>
+			<button type="reset" class="btn"><span>Reset</span></button>
+		</div>
+	</form>
+</div>
